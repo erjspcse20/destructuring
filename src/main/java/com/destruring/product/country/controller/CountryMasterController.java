7a0925de0cbef440f51b21aefb921f9e42970a44
jai PANDEY <jaipandey@jais-MacBook-Air.local>
2018-10-26 08:19:56 +0530

    chnging

--- src/main/java/com/destruring/product/country/controller/CountryMasterController.java
+++ src/main/java/com/destruring/product/country/controller/CountryMasterController.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,7 +42,11 @@ public class CountryMasterController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/addCountry")
-	public void addCountry(CountryModel country) {
+	public void addCountry(@RequestBody CountryModel country) {
+		System.out.println("createdby"+country.getCreatedBy());
+		System.out.println("active"+country.getIsActive());
+		System.out.println("description"+country.getDescription());
+		System.out.println("name"+country.getName());
 		UUID uuid = UUID.randomUUID();
         String randomUUIDString = uuid.toString();
         Date currentdate=new Date();
